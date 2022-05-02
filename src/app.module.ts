b9107152c8ed9b2bdc7aa7c8ef428fc21e76ab4e
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-05-02 19:41:46 -0300

    add module-alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { UserModule } from '@app/user/user.module';
 
 @Module({
   imports: [UserModule],
