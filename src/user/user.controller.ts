362c989ea85aa3e42aa9611bf338a27b9a67060c
Juan Ignacio Costamagna <juan.costamagna@mercadolibre.com>
2022-05-04 22:01:36 -0300

    add login user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { LoginUserDto } from './dto/loginUser.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
 import { User } from './user.entity';
 import { UserService } from './user.service';
@@ -17,6 +18,14 @@ export class UserController {
   @UsePipes(new ValidationPipe())
   async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
       const user = await this.userService.createUser(createUserDto);
+
+      return this.userService.buildUserResponse(user);
+  }
+
+  @Post('/login')
+  @UsePipes(new ValidationPipe())
+  async logUser(@Body('user') loginUserDto: LoginUserDto): Promise<UserResponseInterface> {
+      const user = await this.userService.login(loginUserDto);
 
       return this.userService.buildUserResponse(user);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,8 @@ import { User } from './user.entity';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -52,5 +54,24 @@ export class UserService {
                 token: this.generateJwt(user)
             }
         }
+    }
+
+    async login(loginUserDto: LoginUserDto): Promise<User> {
+        const userByUsername = await this.userRepository.findOne({
+            username: loginUserDto.username
+        });
+
+        if (!userByUsername) {
+            throw new HttpException('Username does not exist', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        const isPasswordCorrect = await compare(loginUserDto.password, userByUsername.password);
+
+        if (!isPasswordCorrect) {
+            throw new HttpException('Username or password are not correct', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        delete userByUsername.password;
+        return userByUsername;
     }
 }
